6fb8131958b2b6828c1e0736577de2144c3634e0
tejuypatil <tejuypatil@gmail.com>
2022-08-29 13:50:04 +0530

    Started Employee payroll service program

---
+++ src/test/java/com/development/employeepayrollapp/EmployeePayrollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.development.employeepayrollapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
