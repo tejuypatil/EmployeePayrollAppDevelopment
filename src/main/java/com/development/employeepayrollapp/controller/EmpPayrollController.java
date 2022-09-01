d1505d43a32098ca04af7bc8fa48df84c598f070
tejuypatil <tejuypatil@gmail.com>
2022-09-01 21:35:44 +0530

    Added Validation to Name Field so the REST call can be validated

--- src/main/java/com/development/employeepayrollapp/controller/EmpPayrollController.java
+++ src/main/java/com/development/employeepayrollapp/controller/EmpPayrollController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 
 @RestController
@@ -34,7 +35,7 @@ public class EmpPayrollController {
     }
 
     @PostMapping("/payroll")
-    public ResponseEntity<ResponseDTO> addEmployeePayrollData(@RequestBody EmpPayrollDTO empPayrollDTO) {
+    public ResponseEntity<ResponseDTO> addEmployeePayrollData(@Valid @RequestBody EmpPayrollDTO empPayrollDTO) {
         EmpPayrollData empPayrollData = empPayrollService.create(empPayrollDTO);
         ResponseDTO responseDTO = new ResponseDTO("Get call success", empPayrollData);
         ResponseEntity<ResponseDTO> response = new ResponseEntity<>(responseDTO, HttpStatus.OK);
